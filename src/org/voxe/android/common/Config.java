3213f50e52c10983fc4140542e65a4c5fb5ef254
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-01-20 20:30:02 +0100

    adding disabling of loading at release

---
+++ src/org/voxe/android/common/Config.java
@@ -0,0 +1,12 @@
+package org.voxe.android.common;
+
+public abstract class Config {
+	
+	public static final boolean RELEASE = false;
+	
+	public static final boolean LOG_TO_CONSOLE = RELEASE;
+
+	private Config() {
+	}
+	
+}
--- src/org/voxe/android/common/LogHelper.java
+++ src/org/voxe/android/common/LogHelper.java
@@ -1,23 +1,27 @@
 package org.voxe.android.common;
 
 import static java.lang.System.currentTimeMillis;
+import static org.voxe.android.common.Config.LOG_TO_CONSOLE;
 import android.util.Log;
 
-/*
- * TODO Disable logs for release
- */
 public class LogHelper {
 
 	public static void logException(String message, Exception exception) {
-		Log.e("TheVoxe", message, exception);
+		if (LOG_TO_CONSOLE) {
+			Log.e("TheVoxe", message, exception);
+		}
 	}
 
 	public static void log(String message) {
-		Log.d("TheVoxe", message);
+		if (LOG_TO_CONSOLE) {
+			Log.d("TheVoxe", message);
+		}
 	}
 
 	public static void logDuration(String message, long startTimestamp) {
-		log(message + " Duration in ms: " + (currentTimeMillis() - startTimestamp));
+		if (LOG_TO_CONSOLE) {
+			log(message + " Duration in ms: " + (currentTimeMillis() - startTimestamp));
+		}
 	}
 
 }
